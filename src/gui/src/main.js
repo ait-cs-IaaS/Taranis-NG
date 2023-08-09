1244fcdc8f67f8b2b7e91af23274674144cfda5d
Benjamin Akhras <b@akhras.at>
2023-08-09 08:37:04 +0200

    add sentry support

--- src/gui/src/main.js
+++ src/gui/src/main.js
@@ -9,6 +9,7 @@ import { vuetify } from '@/plugins/vuetify'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import 'vue-datepicker-next/index.css'
+import * as Sentry from '@sentry/vue'
 
 export const app = createApp(App)
 app.use(DatePicker)
@@ -22,6 +23,26 @@ const coreAPIURL =
 
 ApiService.init(coreAPIURL)
 app.provide('$coreAPIURL', coreAPIURL)
+
+const sentryDsn = import.meta.env.VITE_TARANIS_NG_SENTRY_DSN
+
+if (sentryDsn) {
+  Sentry.init({
+    app,
+    dsn: sentryDsn,
+    autoSessionTracking: true,
+    integrations: [
+      new Sentry.BrowserTracing({
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router)
+      }),
+      new Sentry.Replay()
+    ],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0
+  })
+}
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
