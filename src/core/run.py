61a1fb03f53803921fa2e6143e1c174cdb312b1a
Benjamin Akhras <b@akhras.at>
2023-08-22 15:10:24 +0200

    cleanup docker folder fix #141 & #142

--- src/core/run.py
+++ src/core/run.py
@@ -1,8 +1,14 @@
 #! /usr/bin/env python
 
 from core import create_app
+from gunicorn.arbiter import Arbiter
 
 app = create_app()
+
+
+def post_worker_init(worker):
+    print(worker.pid)
+
 
 if __name__ == "__main__":
     app.run()
