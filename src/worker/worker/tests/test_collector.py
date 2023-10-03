b3f3309c77371122bc2134b51e1060b070b702ef
ewoiset <elisabeth.woisetschlaeger@ait.ac.at>
2023-10-03 12:34:23 +0200

    Add different testing cases

--- src/worker/worker/tests/test_collector.py
+++ src/worker/worker/tests/test_collector.py
@@ -13,29 +13,41 @@ def test_initalize_collectors():
     collectors.RSSCollector()
 
 
-def test_filter_by_word_list_empty_wordlist(rss_collector):
-    whitelist_results = (
-        rss_collector.filter_by_word_list(news_items, source_empty_wordlist))
+@pytest.mark.parametrize("input_news_items",
+                         [news_items, news_items[2:], news_items[::len(news_items) - 1], [news_items[-1]]])
+def test_filter_by_word_list_empty_wordlist(rss_collector, input_news_items):
+    emptylist_results = (
+        rss_collector.filter_by_word_list(input_news_items, source_empty_wordlist))
 
-    assert whitelist_results == news_items
+    assert emptylist_results == input_news_items
 
 
-def test_filter_by_word_list_include_list(rss_collector):
+@pytest.mark.parametrize("input_news_items,expected_news_items",
+                         [(news_items, news_items[:2]), (news_items[2:], []),
+                          (news_items[::len(news_items) - 1], [news_items[0]]), ([news_items[-1]], [])])
+def test_filter_by_word_list_include_list(rss_collector, input_news_items, expected_news_items):
     include_list_results = (
-        rss_collector.filter_by_word_list(news_items, source_include_list))
+        rss_collector.filter_by_word_list(input_news_items, source_include_list))
 
-    assert include_list_results == news_items[:2]
+    assert include_list_results == expected_news_items
 
 
-def test_filter_by_word_list_exclude_list(rss_collector):
+@pytest.mark.parametrize("input_news_items,expected_news_items",
+                         [(news_items, news_items[::len(news_items) - 1]), (news_items[2:], [news_items[-1]]),
+                          (news_items[::len(news_items) - 1], news_items[::len(news_items) - 1]),
+                          ([news_items[-1]], [news_items[-1]])])
+def test_filter_by_word_list_exclude_list(rss_collector, input_news_items, expected_news_items):
     exclude_list_results = (
-        rss_collector.filter_by_word_list(news_items, source_exclude_list))
+        rss_collector.filter_by_word_list(input_news_items, source_exclude_list))
 
-    assert exclude_list_results == news_items[::len(news_items) - 1]
+    assert exclude_list_results == expected_news_items
 
 
-def test_filter_by_word_list_include_exclude_list(rss_collector):
+@pytest.mark.parametrize("input_news_items,expected_news_items",
+                         [(news_items, [news_items[0]]), (news_items[2:], []),
+                          (news_items[::len(news_items) - 1], [news_items[0]]), ([news_items[-1]], [])])
+def test_filter_by_word_list_include_exclude_list(rss_collector, input_news_items, expected_news_items):
     include_exclude_list_results = (
-        rss_collector.filter_by_word_list(news_items, source_include_list_exclude_list))
+        rss_collector.filter_by_word_list(input_news_items, source_include_list_exclude_list))
 
-    assert include_exclude_list_results == [news_items[0]]
+    assert include_exclude_list_results == expected_news_items
